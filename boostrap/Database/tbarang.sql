3d7d2b452ec65f3353dfce1ab97018c6907b74e9
nayyas <aisyahananda39@gmail.com>
2024-09-23 10:14:57 +0700

    Add files via upload

---
+++ boostrap/Database/tbarang.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 09, 2024 at 08:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcrud-boostrap`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbarang`
+--
+
+CREATE TABLE `tbarang` (
+  `id_barang` int(11) NOT NULL,
+  `kode` varchar(15) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `asal` varchar(25) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `satuan` varchar(15) NOT NULL,
+  `tanggal_diterima` date NOT NULL,
+  `tanggal_simpan` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbarang`
+--
+
+INSERT INTO `tbarang` (`id_barang`, `kode`, `nama`, `asal`, `username`, `jumlah`, `satuan`, `tanggal_diterima`, `tanggal_simpan`) VALUES
+(1, 'INV-2022-001', 'Meja Kantor', 'Pembeli', 'Julia', 1, 'Unit', '2024-08-14', '2024-08-26 00:49:51'),
+(2, 'INV-2022-002', 'Komputer', 'Sumbangan', 'Charlie', 13, 'unit', '2023-02-17', '2024-08-26 00:49:51');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbarang`
+--
+ALTER TABLE `tbarang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbarang`
+--
+ALTER TABLE `tbarang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
